78cabb712418ccfe792837a48073dd8b81470325
Sergio R. Caprile <scaprile@gmail.com>
2022-09-24 20:44:45 -0300

    Add some comments

--- gentry.py
+++ gentry.py
@@ -37,22 +37,29 @@ def status_build(ts, left, right, out1, out2):
     }
     return msg
 
+# state variables
 left = False
 right = False
 out1 = False
 out2 = False
 
+# status callback
 def status_buildmessage():
     return status_build(now(), left, right, out1, out2)
 
 
+# init relays
 GPIO.setmode(GPIO.BCM)
 for i in range(4):
     GPIO.setup(relay[i], GPIO.OUT)
+# init sensors
+# init MQTT client
 client = Client(status_buildmessage)
+# init timers
 idletime = now() + TIME_IDLE
 activetime = 0
 inactivetime = 0
+# init state variables
 out1 = True
 oldout = out2
 oldright = right
@@ -60,6 +67,7 @@ oldleft = left
 changes = False
 
 while True:
+    # detect changes
     if right != oldright :
         oldright = right
         print("right: " + "detect" if right else "idle")
@@ -72,20 +80,28 @@ while True:
         oldout = out2
         print("out2: " + "on" if out2 else "off")
         changes = True
+    # report changes
     if changes :
         changes = False
         idletime = now() + TIME_IDLE
         client.sendstatus("activity")
+    # report if idle for too long
     if timerexpired(idletime):
         idletime = now() + TIME_IDLE
         client.sendstatus("idle")
+    # set output off on inactivity
     if timerexpired(activetime):
         out2 = False
         inactivetime = time.time() + TIME_ACTIVE
+    # set output on on activity
     if left or right:
         activetime = now() + TIME_ACTIVE
         if time.time() > inactivetime : out2 = True
-    GPIO.output(relay[i], GPIO.HIGH if out2 else GPIO.LOW)
+    # relays follow outputs
+    GPIO.output(relay[0], GPIO.HIGH if out1 else GPIO.LOW)
+    GPIO.output(relay[1], GPIO.HIGH if out2 else GPIO.LOW)
+    # process sensors
+    # sleep for a while
     time.sleep(0.1)
 
 GPIO.cleanup()
